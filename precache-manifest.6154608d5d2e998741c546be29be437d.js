7e2ffd1811eb4f339771025748add76aebd69c33
Edward <35878962+eduard20028@users.noreply.github.com>
2020-02-17 20:44:11 +0200

    Updates

---
+++ precache-manifest.6154608d5d2e998741c546be29be437d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462b5f36d55d881b21d870e959b0b6c8",
+    "url": "/drinks-db/index.html"
+  },
+  {
+    "revision": "45abd315abbe77691dc0",
+    "url": "/drinks-db/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "b3b3f7e4572c409efdc2",
+    "url": "/drinks-db/static/css/main.fabc8a74.chunk.css"
+  },
+  {
+    "revision": "45abd315abbe77691dc0",
+    "url": "/drinks-db/static/js/2.1a9043c9.chunk.js"
+  },
+  {
+    "revision": "1c7f0ad687308896ffe8c0ba36060daa",
+    "url": "/drinks-db/static/js/2.1a9043c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b3f7e4572c409efdc2",
+    "url": "/drinks-db/static/js/main.4073a940.chunk.js"
+  },
+  {
+    "revision": "172cd0e0fb2d8683a460",
+    "url": "/drinks-db/static/js/runtime-main.52de8846.js"
+  }
+]);
